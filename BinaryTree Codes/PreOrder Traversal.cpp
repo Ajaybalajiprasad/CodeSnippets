d83a12a41b538377c31991e51fd189cd545eac50
kodliebe <ajaybalajiprasad02@gmail.com>
2024-03-17 18:13:31 +0530

    PreOrder Traversal

---
+++ BinaryTree Codes/PreOrder Traversal.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+    int data;
+    TreeNode* right;
+    TreeNode* left;
+
+    TreeNode(int val) : data(val), right(nullptr), left(nullptr){};
+};
+
+class BinaryTree{
+public:
+
+    void PreOrder(TreeNode* root){
+        if(root==nullptr)return;
+
+        cout<<root->data<<" ";
+        PreOrder(root->left);
+        PreOrder(root->right);
+    }
+};
+
+int main() {
+    BinaryTree bt;
+
+    TreeNode* root = new TreeNode(1);
+    root -> left = new TreeNode(2);
+    root -> right = new TreeNode(3);
+    root -> left -> left = new TreeNode(4);
+    root -> left -> right = new TreeNode(5);
+    root -> left -> right -> left = new TreeNode(8);
+    root -> right -> left = new TreeNode(6);
+    root -> right -> right = new TreeNode(7);
+    root -> right -> right -> left = new TreeNode(9);
+    root -> right -> right -> right = new TreeNode(10);
+
+
+    cout<<"PreOrder Traversal : ";
+    bt.PreOrder(root);
+    cout<<endl;
+return 0;
+}
